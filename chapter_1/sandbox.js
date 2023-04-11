6a00c71669e5d07d669573e0f9d6e0eea6d9f943
Steven Duong <stevenduongx@gmail.com>
2023-04-11 02:08:55 -0600

    Update sandbox.js

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -46,3 +46,34 @@ console.log(results);
 
 results = email.slice(4, 10);
 console.log(results);
+
+// numbers
+let radius = 10;
+const pi = 3.14;
+
+console.log(radius, pi);
+
+// math operators +, -, *, /, **, %
+console.log(10 / 2);
+let res = radius % 3;
+let area = pi * radius**2;
+
+console.log(res, area);
+
+let likes = 10;
+likes++;
+likes--;
+likes+=10;
+likes*=2;
+likes-=5;
+likes/=2;
+likes-=0.5;
+
+console.log(likes);
+
+// NaN - not a number
+console.log(5 / 'hello');
+console.log(5 * 'hello');
+
+let strRes = 'the blog has ' + likes + ' likes';
+console.log(strRes);
