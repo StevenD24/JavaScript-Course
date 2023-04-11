e9a00f6c7aa9cc256656e978fc08c10c3e55e7ba
Steven Duong <stevenduongx@gmail.com>
2023-04-11 01:53:00 -0600

    Update sandbox.js

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,10 +1,41 @@
-let age = 25;
-let year = 2019;
+// let age = 25;
+// let year = 2019;
 
-console.log(age, year);
 
-age = 30;
-console.log(age);
+// // log things to console
+// console.log(age, year);
 
-const point = 100;
-console.log(point);
+// age = 30;
+// console.log(age);
+
+// const point = 100;
+// console.log(point);
+
+// var score = 75;
+// console.log(score);
+
+// strings
+console.log('hello world!');
+
+let email = 'steven@gmail.com';
+console.log(email);
+
+// string concatenation
+let firstName = 'Steven';
+let lastName = 'Duong';
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[0]);
+
+// string length
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index);
