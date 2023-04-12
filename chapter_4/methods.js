f16ada61fc7fdfb0c7be94506c683530006661a1
Steven Duong <stevenduongx@gmail.com>
2023-04-11 18:57:50 -0600

    Update methods.js

---
+++ chapter_4/methods.js
@@ -0,0 +1,15 @@
+// function declarations
+function greet() {
+    console.log('hello world');
+}
+
+// function expression
+const speak = function() {
+    console.log('good day!');
+};
+
+// function call
+greet();
+
+// invoke function expression
+speak();
