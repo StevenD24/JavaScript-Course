4a37f09f01b85d4675c4691b07094fdd1c4f88c7
Steven Duong <stevenduongx@gmail.com>
2023-04-18 09:54:27 -0700

    Update sandbox4.js

--- chapter_6/sandbox4.js
+++ chapter_6/sandbox4.js
@@ -1,9 +1,20 @@
+// query the button
 const button = document.querySelector('button');
+const ul = document.querySelector('ul');
 
 // fires the callback function when the button is clicked
-button.addEventListener('click', () => {
-    console.log('you clicked me');
+button.addEventListener('click', e => {
+    // ul.innerHTML += '<li>something new</li>';
+    
+    // different way to achieve the above result
+    const li = document.createElement('li');
+    li.textContent = 'something new to do';
+    // ul.append(li); // put at the bottom
+    ul.prepend(li); // put at the top
 });
+
+// const ul = document.querySelector('ul');
+// ul.remove();
 
 // want all of the li tags
 // gives us a node list
@@ -18,5 +29,7 @@ items.forEach(item => {
         console.log(e.target);
         // puts a line through the text when the item is clicked
         e.target.style.textDecoration = 'line-through';
+        
+        e.target.remove();
     });
 });
