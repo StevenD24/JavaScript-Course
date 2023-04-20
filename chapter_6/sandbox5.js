825dea2e014411cd6e2c6bfd46410050b1790dff
Steven Duong <stevenduongx@gmail.com>
2023-04-19 17:27:08 -0700

    ch6 complete

---
+++ chapter_6/sandbox5.js
@@ -0,0 +1,19 @@
+// *-- DOM Events --*
+
+const copy = document.querySelector('.copy-me');
+
+copy.addEventListener('copy', () => {
+    console.log("OI! my content is copyright");
+});
+
+const box = document.querySelector('.box');
+
+box.addEventListener('mousemove', e => {
+    //  console.log(e);
+    // console.log(e.offsetX, e.offsetY);
+    box.textContent = `x position - ${e.offsetX} y pos - ${e.offsetY}`;
+});
+
+document.addEventListener('wheel', e => {
+    console.log(e.pageX, e.pageY);
+});
