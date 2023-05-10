7c8eb1d31386589b26dacdaf305fdc350ac60e05
Steven Duong <stevenduongx@gmail.com>
2023-05-10 17:40:11 -0600

    Update formEvents.js

--- chapter_7/formEvents.js
+++ chapter_7/formEvents.js
@@ -1,9 +1,26 @@
 const form = document.querySelector('.signup-form');
-const username = document.querySelector('#username');
+// const username = document.querySelector('#username');
 
 // take the event parameter in the callback function
 form.addEventListener('submit', e => {
     e.preventDefault(); // prevent the default action (refreshing)
-    console.log(username.value); // gets the value in the textbox
+    // console.log(username.value); // gets the value in the textbox
     console.log(form.username.value);
 });
+
+// testing Regex
+const username = 'steven';
+const pattern = /^[a-z]{6,}$/
+
+// let result = pattern.test(username);
+// console.log(result);
+
+// if(result) {
+//     console.log('regex test passed')
+// } else {
+//     console.log('regex test failed')
+// }
+
+// returns an integer
+let result = username.search(pattern);
+console.log(result);
