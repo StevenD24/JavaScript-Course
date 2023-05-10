c06bccb06f6cf7865a6fc8193e99ca2a012c1e7f
Steven Duong <stevenduongx@gmail.com>
2023-05-10 17:48:31 -0600

    update validations

--- chapter_7/formEvents.js
+++ chapter_7/formEvents.js
@@ -1,16 +1,29 @@
 const form = document.querySelector('.signup-form');
+const feedback = document.querySelector('.feedback');
 // const username = document.querySelector('#username');
 
 // take the event parameter in the callback function
 form.addEventListener('submit', e => {
     e.preventDefault(); // prevent the default action (refreshing)
     // console.log(username.value); // gets the value in the textbox
-    console.log(form.username.value);
+    // console.log(form.username.value);
+
+    // form validation using regex
+    const username = form.username.value;
+    const usernamePattern = /^[a-z]{6,12}$/;
+
+    if (usernamePattern.test(username)) {
+        // feedback good info
+        feedback.textContent = 'that username is valid!';
+    } else {
+        // feedback help info
+        feedback.textContent = 'username must contain letters only and must be between 6 & 12 characters long';
+    }
 });
 
 // testing Regex
-const username = 'steven';
-const pattern = /^[a-z]{6,}$/
+// const username = 'steven';
+// const pattern = /^[a-z]{6,}$/
 
 // let result = pattern.test(username);
 // console.log(result);
@@ -22,5 +35,5 @@ const pattern = /^[a-z]{6,}$/
 // }
 
 // returns an integer
-let result = username.search(pattern);
-console.log(result);
+// let result = username.search(pattern);
+// console.log(result);
