78c78ff05495431d883a61947729216427da43da
Steven Duong <stevenduongx@gmail.com>
2023-05-10 19:41:11 -0600

    Update sandbox.js

--- chapter_8/sandbox.js
+++ chapter_8/sandbox.js
@@ -1,5 +1,8 @@
 const scores = [10, 30, 15, 25, 50, 40, 5];
 
-scores.filter((score) => {
+// does not alter the original array
+const filteredScores = scores.filter((score) => {
     return score > 20;
 });
+
+console.log(filteredScores);
