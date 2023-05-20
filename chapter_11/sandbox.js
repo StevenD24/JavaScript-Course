1cbb464903fc93eb5fc7719c7b0b97abfd3f0f69
Steven Duong <stevenduongx@gmail.com>
2023-05-20 16:49:08 -0600

    create ch11

---
+++ chapter_11/sandbox.js
@@ -0,0 +1,10 @@
+// dates & times
+// call the constructor
+const now = new Date();
+
+console.log(now);
+console.log(typeof now);
+
+// year, months, day, times
+console.log('getFullYear', now.getFullYear());
+console.log('getMonth', now.getFullYear());
