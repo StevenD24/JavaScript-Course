9c0ebea42aeafda36b0d5c9982e3ea67819214aa
Steven Duong <stevenduongx@gmail.com>
2023-05-20 22:13:21 -0600

    update ch 12

--- chapter_12/app.js
+++ chapter_12/app.js
@@ -1,30 +1,70 @@
-const getTodos = (callback) => {
-    const request = new XMLHttpRequest();
+const getTodos = (resource) => {
 
-    request.addEventListener('readystatechange', () => {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+
+        request.addEventListener('readystatechange', () => {
         if (request.readyState === 4 && request.status === 200) {
-            callback(undefined, request.responseText);
+            const data = JSON.parse(request.responseText);
+            resolve(data);
         } else if (request.readyState === 4) {
-            callback('could not fetch data', undefined);
+            reject('error getting resource');
         }
     });
-
-    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-    request.send();
+    
+        request.open('GET', resource);
+        request.send();
+    });
 };
+
+getTodos('todos/steven.json').then(data => {
+    console.log('promise resolved: ', data);
+}).catch(err => {
+    console.log('promise rejected: ', err);
+});
 
 console.log(1);
 console.log(2);
 
 // async request is not blocking the code below.
-getTodos((err, data) => {
-    console.log('callback fired');
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
-});
+// this is an example of callback hell
+// if u need to call an API and use that info to call another API
+// getTodos('todos/steven.json', (err, data) => {
+//     console.log(data);
+//     getTodos('todos/lin.json', (err, data) => {
+//         console.log(data);
+//         getTodos('todos/kevin.json', (err, data) => {
+//             console.log(data);
+//         });
+//     });
+//     // if (err) {
+//     //     console.log(err);
+//     // } else {
+//     //     console.log(data);
+//     // }
+// });
+
+// const getSomething = () => {
+//     return new Promise((resolve, reject) => {
+//         // fetch something
+//         resolve('some data');
+//         // reject('some error');
+//     });
+// };
+
+// getSomething().then((data) => {
+//     console.log(data);
+// }, (err) => {
+//     console.log(err);
+// });
+
+// getSomething().then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// });
 
 console.log(3);
 console.log(4);
+
+// JSON are strings that looks like JS objects
