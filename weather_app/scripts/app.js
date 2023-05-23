7b78010c0bbd3581ef294b3de33cf1d6a29d4780
Steven Duong <stevenduongx@gmail.com>
2023-05-22 21:44:04 -0600

    update app.js

--- weather_app/scripts/app.js
+++ weather_app/scripts/app.js
@@ -1,6 +1,8 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUI = (data) => {
 
@@ -27,6 +29,18 @@ const updateUI = (data) => {
         </div>
     </div>
 `;
+
+    // update the day/night icon images
+    const iconSrc = `img/icons/${weather[0].WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    let timeSrc = null;
+    if(weather[0].IsDayTime){
+      timeSrc = 'img/day.svg';
+    } else {
+      timeSrc = 'img/night.svg';
+    }
+    time.setAttribute('src', timeSrc);
 
     // remove the d-none class if present
     if (card.classList.contains('d-none')) {
